1e71152f09cae3c44550fc9fac33ad8e53261621
kankyouhakai <kankyouhakai096@gmail.com>
2024-09-30 14:41:44 +0900

    fix sycle

--- sycle.c
+++ sycle.c
@@ -1,24 +1,6 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
-static int flag = 1;
-void* Sleep(void* arg) {
-    sleep(1);
-    
-    flag = 0;
-
-    return NULL;
-}
-
-
 int main(void) {
-    pthread_t th;
-    register unsigned long long int i = 0;
-    pthread_create(&th, NULL, Sleep, NULL);
-    pthread_detach(th);
-    while(flag){
-        i++;
+    register unsigned long long i = 0;
+    for (i = 0; i < 4000000000; i++){
+
     }
-    printf("%llu\n", i);
-    printf("%llu\n", (unsigned long long)-1);
 }
